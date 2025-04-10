ea0fdac60d66d1127e3f25b6b367ee86a2d7c6d5
Nathan Davey <nathan.davey@nixs.com>
2025-04-10 11:01:12 +0200

    Add basic layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navigation from "@/app/ui/Navigation";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Nathan Davey",
+  description: "Welcome to my website, may you be blessed.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`bg-linear-to-tr from-backgroud to-blue-500 ${geistSans.variable} ${geistMono.variable} antialiased grid grid-flow-col items-center justify-items-center p-5 min-h-screen`}
+      >
+        <div className="items-center bg-greyterm max-w-7xl size-full shadow-[12px_12px_rgba(0,0,0,1)] flex flex-col">
+          <Navigation />
+          <div className="p-5 size-full">
+            <div className="border border-white size-full">{children}</div>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center size-full justify-center">
+      <p className=" text-center">My Website</p>
+    </main>
+  );
+}
